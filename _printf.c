2311e05c502457edc654c2ab6f2e09569f91a155
Shadenm-404 <viuxwl7@gmail.com>
2025-10-01 07:21:53 +0000

    Task 0: implement _printf with %c, %s, %% and fix Betty style

--- _printf.c
+++ _printf.c
@@ -13,50 +13,50 @@ int _printf(const char *format, ...)
     char *str;
 
     if (format == NULL)
-        return (-1);
+	return (-1);
 
     va_start(args, format);
 
     while (format[i] != '\0')
     {
-        if (format[i] == '%')
-        {
-            i++;
-            if (format[i] == '\0')
-            {
-                va_end(args);
-                return (-1);
-            }
-            if (format[i] == 'c')
-            {
-                count += _putchar(va_arg(args, int));
-            }
-            else if (format[i] == 's')
-            {
-                str = va_arg(args, char *);
-                if (str == NULL)
-                    str = "(null)";
-                while (*str)
-                {
-                    count += _putchar(*str);
-                    str++;
-                }
-            }
-            else if (format[i] == '%')
-            {
-                count += _putchar('%');
-            }
-            else
-            {
-                count += _putchar('%');
-                count += _putchar(format[i]);
-            }
-        }
-        else
-        {
-            count += _putchar(format[i]);
-        }
-        i++;
+	if (format[i] == '%')
+	{
+	    i++;
+	    if (format[i] == '\0')
+	    {
+		va_end(args);
+		return (-1);
+	    }
+	    if (format[i] == 'c')
+	    {
+		count += _putchar(va_arg(args, int));
+	    }
+	    else if (format[i] == 's')
+	    {
+		str = va_arg(args, char *);
+		if (str == NULL)
+		    str = "(null)";
+		while (*str)
+		{
+		    count += _putchar(*str);
+		    str++;
+		}
+	    }
+	    else if (format[i] == '%')
+	    {
+		count += _putchar('%');
+	    }
+	    else
+	    {
+		count += _putchar('%');
+		count += _putchar(format[i]);
+	    }
+	}
+	else
+	{
+	    count += _putchar(format[i]);
+	}
+	i++;
     }
 
     va_end(args);
