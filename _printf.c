c73d13a5830240a695fd276cb8654f4e58143df1
Shadenm-404 <viuxwl7@gmail.com>
2025-10-03 22:29:53 +0000

    Task 3: add %b (binary) to _printf; Betty-compliant

--- _printf.c
+++ _printf.c
@@ -1,4 +1,87 @@
+cat > _printf.c <<'EOF'
 #include "main.h"
+#include <limits.h>
+
+/**
+ * print_string - prints a string (handles NULL)
+ * @s: string to print
+ * Return: number of characters printed
+ */
+static int print_string(char *s)
+{
+	int n = 0;
+
+	if (s == NULL)
+		s = "(null)";
+	while (*s)
+	{
+		n += _putchar(*s);
+		s++;
+	}
+	return (n);
+}
+
+/**
+ * print_number - prints a signed decimal integer
+ * @n: number to print
+ * Return: number of characters printed
+ */
+static int print_number(int n)
+{
+	int count = 0, i = 0, k, num = n;
+	char buf[12];
+
+	if (num == 0)
+		return (_putchar('0'));
+
+	if (num < 0)
+	{
+		count += _putchar('-');
+		if (num == INT_MIN)
+		{
+			count += _putchar('2');
+			num = 147483648;
+		}
+		else
+		{
+			num = -num;
+		}
+	}
+
+	while (num > 0)
+	{
+		buf[i++] = (char)('0' + (num % 10));
+		num /= 10;
+	}
+	for (k = i - 1; k >= 0; k--)
+		count += _putchar(buf[k]);
+
+	return (count);
+}
+
+/**
+ * print_binary - prints an unsigned int as binary (base 2)
+ * @n: number to print
+ * Return: number of characters printed
+ */
+static int print_binary(unsigned int n)
+{
+	int count = 0, i = 0, k;
+	char buf[32];
+
+	if (n == 0)
+		return (_putchar('0'));
+
+	while (n > 0)
+	{
+		buf[i++] = (char)('0' + (n & 1));
+		n >>= 1;
+	}
+	for (k = i - 1; k >= 0; k--)
+		count += _putchar(buf[k]);
+
+	return (count);
+}
 
 /**
  * _printf - produces output according to a format
@@ -10,7 +93,6 @@ int _printf(const char *format, ...)
 {
 	va_list ap;
 	int i = 0, count = 0;
-	char *s;
 
 	if (format == NULL)
 		return (-1);
@@ -23,16 +105,15 @@ int _printf(const char *format, ...)
 			i++;
 			if (format[i] == '\0')
 				return (va_end(ap), -1);
+
 			if (format[i] == 'c')
 				count += _putchar(va_arg(ap, int));
 			else if (format[i] == 's')
-			{
-				s = va_arg(ap, char *);
-				if (s == NULL)
-					s = "(null)";
-				while (*s)
-					count += _putchar(*s++);
-			}
+				count += print_string(va_arg(ap, char *));
+			else if (format[i] == 'd' || format[i] == 'i')
+				count += print_number(va_arg(ap, int));
+			else if (format[i] == 'b')
+				count += print_binary(va_arg(ap, unsigned int));
 			else if (format[i] == '%')
 				count += _putchar('%');
 			else
@@ -42,9 +123,13 @@ int _printf(const char *format, ...)
 			}
 		}
 		else
+		{
 			count += _putchar(format[i]);
+		}
 		i++;
 	}
 	va_end(ap);
 	return (count);
+}
+EOF
 }
