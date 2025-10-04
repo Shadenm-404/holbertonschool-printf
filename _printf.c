668ebef1e6b46ac00abfcb133057fe3c3edc1d3a
Shadenm-404 <viuxwl7@gmail.com>
2025-10-04 20:52:07 +0000

    Fix printf flags handling (+, space, #, %)

--- _printf.c
+++ _printf.c
@@ -1,82 +1,105 @@
 #include "main.h"
+#include <stdarg.h>
 #include <limits.h>
 
-/* helper to print strings */
+/**
+ * _putchar - writes a character to stdout
+ * @c: The character to print
+ * Return: 1 on success
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+/**
+ * print_string - prints a string
+ * @s: string to print
+ * Return: number of chars printed
+ */
 static int print_string(char *s)
 {
-	int n = 0;
+	int count = 0;
 	if (!s)
 		s = "(null)";
 	while (*s)
-		n += _putchar(*s++);
-	return (n);
+		count += _putchar(*s++);
+	return (count);
 }
 
-/* helper to print numbers */
-static int print_number(int n, int plus_flag, int space_flag)
+/**
+ * print_number - prints integer with flags (+, space)
+ * @n: number to print
+ * @plus: if + flag
+ * @space: if space flag
+ * Return: number of chars printed
+ */
+static int print_number(int n, int plus, int space)
 {
 	int count = 0;
 	unsigned int num;
 
 	if (n >= 0)
 	{
-		if (plus_flag)
+		if (plus)
 			count += _putchar('+');
-		else if (space_flag)
+		else if (space)
 			count += _putchar(' ');
+		num = n;
 	}
 	else
 	{
 		count += _putchar('-');
 		num = -n;
 	}
-	if (n < 0)
-		num = -n;
-	else
-		num = n;
-
 	if (num / 10)
 		count += print_number(num / 10, 0, 0);
 	count += _putchar((num % 10) + '0');
 	return (count);
 }
 
-/* helper for %# flag */
-static int print_with_hash(unsigned int n, char type)
+/**
+ * print_unsigned - prints unsigned integer in base
+ * @n: number
+ * @base: base (8, 10, 16)
+ * @hash: if # flag
+ * @type: type for hex case (x, X)
+ * Return: count
+ */
+static int print_unsigned(unsigned int n, int base, int hash, char type)
 {
 	int count = 0;
+	char digits[] = "0123456789abcdef";
+	char digitsX[] = "0123456789ABCDEF";
 
-	if (type == 'x' && n != 0)
-		count += print_string("0x");
-	else if (type == 'X' && n != 0)
-		count += print_string("0X");
-	else if (type == 'o' && n != 0)
-		count += _putchar('0');
-
-	if (type == 'x' || type == 'X')
+	if (hash && n != 0)
 	{
-		char hex[] = "0123456789abcdef";
-		if (type == 'X')
-			for (int i = 10; i < 16; i++)
-				hex[i] = 'A' + (i - 10);
-		if (n / 16)
-			count += print_with_hash(n / 16, type);
-		count += _putchar(hex[n % 16]);
+		if (base == 8)
+			count += _putchar('0');
+		else if (base == 16 && type == 'x')
+			count += write(1, "0x", 2);
+		else if (base == 16 && type == 'X')
+			count += write(1, "0X", 2);
 	}
-	else if (type == 'o')
-	{
-		if (n / 8)
-			count += print_with_hash(n / 8, type);
-		count += _putchar((n % 8) + '0');
-	}
+	if (n / base)
+		count += print_unsigned(n / base, base, 0, type);
+	if (type == 'X')
+		count += _putchar(digitsX[n % base]);
+	else
+		count += _putchar(digits[n % base]);
 	return (count);
 }
 
-/* main printf function */
+/**
+ * _printf - simplified printf
+ * @format: format string
+ * Return: count
+ */
 int _printf(const char *format, ...)
 {
 	va_list ap;
-	int i = 0, count = 0, plus_flag = 0, space_flag = 0, hash_flag = 0;
+	int i = 0, count = 0;
+	int plus, space, hash;
 
 	if (!format)
 		return (-1);
@@ -86,35 +109,38 @@ int _printf(const char *format, ...)
 	{
 		if (format[i] == '%')
 		{
-			i++;
-			plus_flag = space_flag = hash_flag = 0;
-
-			/* detect flags */
+			i++, plus = space = hash = 0;
 			while (format[i] == '+' || format[i] == ' ' || format[i] == '#')
 			{
 				if (format[i] == '+')
-					plus_flag = 1;
+					plus = 1;
 				else if (format[i] == ' ')
-					space_flag = 1;
+					space = 1;
 				else if (format[i] == '#')
-					hash_flag = 1;
+					hash = 1;
 				i++;
 			}
-
 			if (format[i] == 'c')
 				count += _putchar(va_arg(ap, int));
 			else if (format[i] == 's')
 				count += print_string(va_arg(ap, char *));
 			else if (format[i] == 'd' || format[i] == 'i')
-				count += print_number(va_arg(ap, int), plus_flag, space_flag);
-			else if (format[i] == 'x' || format[i] == 'X' || format[i] == 'o')
-				count += print_with_hash(va_arg(ap, unsigned int), format[i]);
+				count += print_number(va_arg(ap, int), plus, space);
+			else if (format[i] == 'u')
+				count += print_unsigned(va_arg(ap, unsigned int), 10, 0, 'u');
+			else if (format[i] == 'o')
+				count += print_unsigned(va_arg(ap, unsigned int), 8, hash, 'o');
+			else if (format[i] == 'x' || format[i] == 'X')
+				count += print_unsigned(va_arg(ap, unsigned int), 16, hash, format[i]);
 			else if (format[i] == '%')
 				count += _putchar('%');
 			else
 			{
 				count += _putchar('%');
-				count += _putchar(format[i]);
+				if (format[i])
+					count += _putchar(format[i]);
+				else
+					return (-1);
 			}
 		}
 		else
